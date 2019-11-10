3e1f1ca3029567960290c7852c56e6db7571a950
BrianDung <tran.tiendung96hust@gmail.com>
2019-11-10 23:11:52 +0700

    Update Method show Person detail

---
+++ TestGithub/src/dung/com/Update.java
@@ -0,0 +1,9 @@
+package dung.com;
+
+public class Update {
+	Person person = new Person();
+	void show(Person person) {
+		person.setName("dung");
+		System.out.println(this.person.getName());
+	}
+}	
